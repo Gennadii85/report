30a998b1ed3f299a8f731ba125171cd9ac4854a4
Gennadii85 <103601269+Gennadii85@users.noreply.github.com>
2024-02-27 23:03:31 +0200

    заведены dataList from holds

--- lib/core/variables_holds.dart
+++ lib/core/variables_holds.dart
@@ -10,38 +10,180 @@ class VarHolds {
   static const String portShipsSideTitle = 'Port ships side';
   static const String tankTopsTitle = 'Tank tops';
 
-  static const List dataHoldsTable = [
+  static const List dataHoldsTableForward = [
     [
       'Plating',
       [
-        'Deck plating was free from damage and was painted, but affected at some places by rust where paint cover was scratched. Appropriate marking was applied.',
-        'The external shell plating was painted, but affected at some places by rust where paint cover was scratched or peel off.',
-        'Deck plating was free from damage and well painted up. Appropriate marking was applied.',
+        'Plating were in satisfactory condition, well painted up and apparently damage free.',
+        'Some areas of scratched/damaged/pilled off paint cover apparent in some places  especially at the lowest part.',
+        'No dent or bent areas were noted.',
+        'Plating was affected with indentation as follow: ____________________________',
+        'Lower frames brackets dented at some place.',
+        'Frame bent as follow:_____________________',
+        'Deficiency:____________________',
       ],
     ],
     [
-      'Ladders, fittings',
-      ['444', '555', '666'],
-    ],
-    [
-      'Plating and frames',
-      ['777Ladders, fittings', '888Ladders, fittings', '999Ladders, fittings'],
-    ],
-    [
-      'Top side plating',
-      ['777Top side plating', '888Top side plating', '999Top side plating'],
-    ],
-    [
-      'Hopper plating',
-      ['777Hopper plating', '888Hopper plating', '999Hopper plating'],
+      'Ladders',
+      [
+        'Apparently ladders and their platforms are damage free and properly maintained up.',
+        'The stay of intermedia vertical ladder was bent.',
+        'The stay of vertical ladder was bent.',
+        'The following rungs of the vertical ladders were bent/torn: Nos____________',
+        'Intermediate platform had some protectors/framework locally bent/damage.',
+        'The intermediate platform was bent.',
+        'Deficiency:____________________',
+      ],
     ],
     [
       'Pipe, protection',
-      ['777Pipe, protection', '888Pipe, protection', '999Pipe, protection'],
+      [
+        'Piping and its protectors were found to be in satisfactory condition, apparently leakage and damage free.',
+        'However following were noted:',
+        'Some pipes supports were missing.',
+        'Some pipes holders were bent.',
+        'Protections of pipes was bent/dented at some places.',
+        'Deficiency:____________________',
+      ],
+    ],
+  ];
+
+  static const List dataHoldsTableStarboard = [
+    [
+      'Plating, framing and fitting',
+      [
+        'Plating were in satisfactory condition, well painted up and apparently damage free.',
+        'Some areas of scratched/damaged/pilled off paint cover apparent in some places  especially at the lowest part.',
+        'Frames bracket stiffeners bent as follow: ______________________________',
+        'Frame bent as follow:_____________________',
+        'Service ladders were damage to a various degree.',
+        'Deficiency:____________________',
+      ],
+    ],
+    [
+      'Top side plating',
+      [
+        'Apparently in satisfactory condition and well coated.',
+        'The steelworks were painted, however some places were affected with rust, where paint was scratched or peel off.',
+        'Plating was affected by indentation as follows: _____________________________',
+      ],
+    ],
+    [
+      'Hopper plating',
+      [
+        'Apparently in satisfactory condition and well coated.',
+        'The steelworks were painted, however some places were affected with rust, where paint was scratched or peel off.',
+        'Plating was affected by indentation as follows: _____________________________',
+      ],
+    ],
+    [
+      'Pipe, protection',
+      [
+        'Piping and its protectors were found to be in satisfactory condition, apparently leakage and damage free.',
+        'However following were noted:',
+        'Some pipes supports were missing.',
+        'Some pipes holders were bent.',
+        'Protections of pipes was bent/dented at some places.',
+        'Deficiency:____________________',
+      ],
+    ],
+  ];
+
+  static const List dataHoldsTableAft = [
+    [
+      'Plating',
+      [
+        'Plating were in satisfactory condition, well painted up and apparently damage free.',
+        'Some areas of scratched/damaged/pilled off paint cover apparent in some places  especially at the lowest part.',
+        'No dent or bent areas were noted.',
+        'Plating was affected with indentation as follow: ____________________________',
+        'Lower frames brackets dented at some place.',
+        'Frame bent as follow:_____________________',
+        'Deficiency:____________________',
+      ],
+    ],
+    [
+      'Ladders',
+      [
+        'Apparently ladders and their platforms are damage free and properly maintained up.',
+        'The stay and protections of vertical ladder at lower part was bent.',
+        'The handrails of the screw-type ladder were bent/torn as follow:____________________.',
+        'Framework of Australian ladders bent.',
+        'Intermediate platform had some protectors/framework locally bent/damage.',
+        'Protections of screw-type ladder was bent/dented at some places.',
+        'The intermediate platform was bent.',
+        'Deficiency:____________________',
+      ],
+    ],
+    [
+      'Pipe, protection',
+      [
+        'Piping and its protectors were found to be in satisfactory condition, apparently leakage and damage free.',
+        'However following were noted:',
+        'Some pipes supports were missing.',
+        'Some pipes holders were bent.',
+        'Protections of pipes was bent/dented at some places.',
+        'Deficiency:____________________',
+      ],
+    ],
+  ];
+
+  static const List dataHoldsTablePort = [
+    [
+      'Plating, framing and fitting',
+      [
+        'Plating were in satisfactory condition, well painted up and apparently damage free.',
+        'Some areas of scratched/damaged/pilled off paint cover apparent in some places  especially at the lowest part.',
+        'Frames bracket stiffeners bent as follow: ______________________________',
+        'Frame bent as follow:_____________________',
+        'Service ladders were damage to a various degree.',
+        'Deficiency:____________________',
+      ],
+    ],
+    [
+      'Top side plating',
+      [
+        'Apparently in satisfactory condition and well coated.',
+        'The steelworks were painted, however some places were affected with rust, where paint was scratched or peel off.',
+        'Plating was affected by indentation as follows: _____________________________',
+      ],
+    ],
+    [
+      'Hopper plating',
+      [
+        'Apparently in satisfactory condition and well coated.',
+        'The steelworks were painted, however some places were affected with rust, where paint was scratched or peel off.',
+        'Plating was affected by indentation as follows: _____________________________',
+      ],
+    ],
+    [
+      'Pipe, protection',
+      [
+        'Piping and its protectors were found to be in satisfactory condition, apparently leakage and damage free.',
+        'However following were noted:',
+        'Some pipes supports were missing.',
+        'Some pipes holders were bent.',
+        'Protections of pipes was bent/dented at some places.',
+        'Deficiency:____________________',
+      ],
+    ],
+  ];
+
+  static const List dataHoldsTableTank = [
+    [
+      'Plating',
+      [
+        'The plating was well painted and apparently in good condition.',
+        'The plating was old painted, locally scratched and affected with powdery rust and scales.',
+        'The plating was not painted, locally scratched and affected with powdery rust and scale.',
+        'Tank top plating was apparently pressed-in between the structural members.',
+      ],
     ],
     [
       'Bilges and covers',
-      ['777Bilges and covers', '888Bilges and covers', '999Bilges and covers'],
+      [
+        'Satisfactory condition. ',
+      ],
     ],
   ];
 }
